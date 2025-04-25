a22b615c4cf0d1d23c06d439501208c079dbdf97
Abdullah <abdullah@Abdullahs-MacBook-Air.local>
2025-04-25 22:41:20 +0600

    added animation manage my post

--- src/PageLayout/ManagePost/ManageMyPost.jsx
+++ src/PageLayout/ManagePost/ManageMyPost.jsx
@@ -1,32 +1,52 @@
 import React from "react";
 import ManagePost from "./ManagePost";
-import { Link } from "react-router-dom";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import PostedVolunteers from "./PostedVolunteers";
+import { motion } from "framer-motion";
+
 const ManageMyPost = () => {
   return (
-    <div className="mt-28">
-      <Tabs >
-        <div className="lg:mx-8 flex justify-center items-center">
+    <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6 }}
+      className="mt-28"
+    >
+      <Tabs>
+        <motion.div
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="lg:mx-8 flex justify-center items-center"
+        >
           <TabList>
             <Tab>My Volunteer Request Post</Tab>
-            <Tab>My volunteer need post</Tab>
+            <Tab>My Volunteer Need Post</Tab>
           </TabList>
-        </div>
+        </motion.div>
+
         <TabPanel>
-          <h2>
-            {" "}
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+          >
             <ManagePost />
-          </h2>
+          </motion.div>
         </TabPanel>
+
         <TabPanel>
-          <h2>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+          >
             <PostedVolunteers />
-          </h2>
+          </motion.div>
         </TabPanel>
       </Tabs>
-    </div>
+    </motion.div>
   );
 };
 
